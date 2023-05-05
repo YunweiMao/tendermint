68814cd3f60193b20453f755c70508e8985f64ea
Yunwei Mao <yunwei@Yunweis-MBP.hsd1.ma.comcast.net>
2023-05-05 11:36:19 -0400

    “initCommand”

--- types/protobuf.go
+++ types/protobuf.go
@@ -1,7 +1,7 @@
 package types
 
 import (
-	abci "github.com/YunweiMao/tendermint/proto/tendermint/abci/types"
+	abci "github.com/YunweiMao/tendermint/proto/tendermint/abci"
 
 	"github.com/YunweiMao/tendermint/crypto"
 	"github.com/YunweiMao/tendermint/crypto/ed25519"
